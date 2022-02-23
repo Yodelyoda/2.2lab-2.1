4a4a0df8a051e131f325b154f5ebcbe65e63230c
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2022-02-23 13:04:28 +0200

    Add files via upload

---
+++ 2lab 2.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _2lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N, j;
+            double S;
+            Console.WriteLine("N = ");
+            N = Convert.ToInt64(Console.Read());
+            S = 0;
+            j = 2;
+            while (j <= N)
+            {
+                S += (j * (N - j)) / (Math.Pow(j, 2) + (Math.Pow(N - j, 2)));
+                j++;
+            }
+            Console.WriteLine("S = ",S);
+            S = 0;
+            j = 2;
+            do
+            {
+                S += (j * (N - j)) / (Math.Pow(j, 2) + (Math.Pow(N - j, 2)));
+                j++;
+            } while (j <= N);
+            Console.WriteLine("S = ", S);
+            S = 0;
+            for (j = 2; j <= N; j++)
+            {
+                S += (j * (N - j)) / (Math.Pow(j, 2) + (Math.Pow(N - j, 2)));
+            }
+            Console.WriteLine("S = ", S);
+            S = 0;
+            for (j = N; j >= 2; j--)
+            {
+                S += (j * (N - j)) / (Math.Pow(j, 2) + (Math.Pow(N - j, 2)));
+            }
+            Console.WriteLine("S = ", S);
+            Console.ReadKey();
+
+        }
+    }
+}
